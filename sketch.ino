b9dc7dfe263eac9c485f53e7898b26cc2d37b4ec
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-07-31 18:07:55 +0200

    Added Wifi connection

--- sketch.ino
+++ sketch.ino
@@ -1,5 +1,18 @@
 #include <Arduino.h>
 #include <TM1637.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include <WiFi.h>
+#include <lwip/netdb.h>
+#include <lwip/sockets.h>
+#include <lwip/dns.h>
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+const char* multicastGroup = "239.12.255.254";
+const int multicastPort = 9522;
 
 #define ENCODER_CLK 2
 #define ENCODER_DT  3
@@ -16,6 +29,8 @@ int pirState = LOW;
 int val = 0;
 
 TM1637 tm(DISPLAY_CLK, DISPLAY_DT);
+
+// API for the percentage of renewable energy in germany: https://api.energy-charts.info/ren_share?country=de
 
 void readEncoder() {
   int clkState = digitalRead(ENCODER_CLK);
@@ -46,11 +61,14 @@ void checkMotion() {
   if (val == HIGH) {
     if (pirState == LOW) {
       Serial1.println("Motion detected!");
+      tm.set(7);
       pirState = HIGH;
     }
   } else {
     if (pirState == HIGH) {
       Serial1.println("Motion ended!");
+      tm.clearDisplay();
+      tm.set(0);
       pirState = LOW;
     }
   }
@@ -66,11 +84,22 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), readEncoder, CHANGE);
   clkLastState = digitalRead(ENCODER_CLK);
   Serial1.println("Started");
+
+  WiFi.begin(ssid, password);
+  Serial1.print("Connecting to WiFi..");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial1.println("Connected");
+  Serial1.print("IP address: ");
+  Serial1.println(WiFi.localIP());
 }
 
 void loop() {
   checkMotion();
-  tm.display(0, (counter / 1000) % 10);
+  if (pirState == HIGH) {
+    tm.display(0, (counter / 1000) % 10);
   tm.display(1, (counter / 100) % 10);
   tm.display(2, (counter / 10) % 10);
   tm.display(3, counter % 10);
@@ -80,5 +109,6 @@ void loop() {
     counter = 0;
   }
 
+  }
   delay(100);
 }
